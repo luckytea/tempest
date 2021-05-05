d40c7356ed384f4e48553492c5993fccb16863e4
Glazkov.A <gearpanda@gmail.com>
2021-05-05 09:32:22 +0300

    update vendoring & golangci config.

--- generator/generator.go
+++ generator/generator.go
@@ -3,19 +3,7 @@ package generator
 
 import (
 	"fmt"
-
-	"github.com/prometheus/prometheus/pkg/labels"
 )
-
-func EmitOpenMetrics(metric Timeseries) (string, error) {
-	switch metric.MetricType {
-	case CounterType:
-		f := fmt.Sprintf("%#v", labels.FromStrings("__name__", "name_here", "code", "400"))
-		return f, nil
-	default:
-		return "", ErrUnsupportedMetricType
-	}
-}
 
 func OpenMetricsLine(metric Timeseries) string {
 	line := fmt.Sprintf(helpTemplate, metric.Name)
--- go.mod
+++ go.mod
@@ -2,11 +2,4 @@ module github.com/LuckyTea/tempest
 
 go 1.16
 
-require (
-	github.com/cespare/xxhash v1.1.0 // indirect
-	github.com/davecgh/go-spew v1.1.1 // indirect
-	github.com/kr/pretty v0.1.0 // indirect
-	github.com/prometheus/prometheus v2.5.0+incompatible
-	github.com/stretchr/testify v1.7.0
-	gopkg.in/check.v1 v1.0.0-20190902080502-41f04d3bba15 // indirect
-)
+require github.com/stretchr/testify v1.7.0
